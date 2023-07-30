558b88030cf8f5399d42cb1db0e32e6f8ab0ccdb
Julian <julian-berner@outlook.de>
2023-07-30 15:44:03 +0200

    fixing column size

--- src/features/kanban-board/KanbanBoard.tsx
+++ src/features/kanban-board/KanbanBoard.tsx
@@ -31,7 +31,7 @@ export default function KanbanBoard({
   return (
     <>
       {tasks.isSuccess && (
-        <div className="h-full w-full bg-gray-900 pt-7 flex px-4 select-none">
+        <div className="h-full pt-7 flex px-4 select-none">
           {board.columns.map((column) => (
             <KanbanBoardColumn
               onAddTaskClick={onAddTaskClick}
@@ -50,11 +50,11 @@ export default function KanbanBoard({
 
 function CreateNewColumnButton({ onClick }: { onClick: () => void }) {
   return (
-    <div className="w-96 mt-2 px-4 mb-16 overflow-hidden">
+    <div className="basis-96 mt-2 px-4 shrink-0 h-[80vh]">
       <Button
         variant="custom"
         onClick={onClick}
-        className="w-full h-full mb-40 bg-slate-800 rounded-xl bg-opacity-20 text-slate-400 mt-11 hover:text-primary-300"
+        className="w-full h-full bg-slate-700 rounded-xl bg-opacity-20 text-slate-400 mt-11 hover:text-primary-300"
       >
         <span className="font-bold text-2xl mr-1">+</span>
         <span className="text-2xl font-bold ">New Column</span>
--- src/features/kanban-board/KanbanBoardColumn.tsx
+++ src/features/kanban-board/KanbanBoardColumn.tsx
@@ -20,7 +20,7 @@ export default function KanbanBoardColumn({
   const ammountOfTask = tasks.length;
 
   return (
-    <div className="px-3 w-96">
+    <div className="px-3 shrink-0 basis-96">
       <h3 className="uppercase font-semibold text-slate-400 tracking-widest mb-6">
         <div className="flex items-center">
           <span className="w-4 h-4 bg-amber-300 rounded-full mr-3"></span>
