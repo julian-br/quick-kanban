484728cafb17b003a947403a540ca17b82504c55
Julian <julian-berner@outlook.de>
2023-09-21 14:58:27 +0200

    adding mock data again

--- src/mock-data/mockData.ts
+++ src/mock-data/mockData.ts
@@ -2,5 +2,5 @@ import { KanbanBoard, Task } from "../api/types";
 import BoardsData from "../mock-data/boardData.json";
 import TaskData from "../mock-data/tasksData.json";
 
-export const boardsData: KanbanBoard[] = [];
+export const boardsData: KanbanBoard[] = BoardsData;
 export const tasksData: Task[] = TaskData;
