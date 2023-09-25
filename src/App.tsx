befe8d59fa00a5884f6521ef2b8fe3fcd2b33780
Julian <julian-berner@outlook.de>
2023-09-25 19:05:32 +0200

    uninstalling react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import KanbanBoardPage from "./pages/KanbanBoardPage";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route } from "wouter";
 import NoCreatedBoardsPage from "./pages/NoCreatedBoardsPage";
 import { AppModalManagerProvider } from "./appModalManager";
 import "@fontsource-variable/nunito";
 
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { retry: false } },
-});
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AppModalManagerProvider>
-        <Route path="/" component={NoCreatedBoardsPage}></Route>
-        <Route path="/board/:boardId">
-          {(params: { boardId: string }) => (
-            <KanbanBoardPage urlParams={params} />
-          )}
-        </Route>
-      </AppModalManagerProvider>
-    </QueryClientProvider>
+    <AppModalManagerProvider>
+      <Route path="/" component={NoCreatedBoardsPage}></Route>
+      <Route path="/board/:boardId">
+        {(params: { boardId: string }) => (
+          <KanbanBoardPage urlParams={params} />
+        )}
+      </Route>
+    </AppModalManagerProvider>
   );
 }
 
